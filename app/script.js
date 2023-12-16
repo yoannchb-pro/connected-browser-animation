1e2b623d80bdf534aa059ff57da9cc393d2b6a85
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2023-12-16 12:21:27 -0500

    connection working

--- app/script.js
+++ app/script.js
@@ -1,94 +1,143 @@
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
+(async function () {
+  const canvas = document.querySelector("canvas");
+  const ctx = canvas.getContext("2d");
 
-let id;
-let width = 0;
-let height = 0;
+  let id;
+  let width = 0;
+  let height = 0;
 
-const setupSize = () => {
-  canvas.width = width = document.body.offsetWidth;
-  canvas.height = height = document.body.offsetHeight;
-};
+  let otherSpheres = [];
 
-const initConnection = async () => {
-  const req = await fetch("http://localhost:3000/register", {
-    method: "POST",
-    body: JSON.stringify({
-      sphereX: window.screenX,
-      sphereY: window.screenY,
-    }),
-  });
-  const res = await req.json();
-  id = res.data.id;
-};
+  let deltaX = Math.floor(window.screen.width * Math.random());
+  let deltaY = Math.floor(window.screen.height * Math.random());
 
-const updatePosition = () => {
-  fetch("http://localhost:3000/update", {
-    method: "POST",
-    body: JSON.stringify({
-      id,
-      sphereX: window.screenX,
-      sphereY: window.screenY,
-    }),
-  });
-};
+  const setupSize = () => {
+    canvas.width = width = document.body.offsetWidth;
+    canvas.height = height = document.body.offsetHeight;
+  };
 
-const closeConnection = () => {
-  fetch("http://localhost:3000/remove", {
-    method: "POST",
-    body: JSON.stringify({
-      id,
-    }),
-  });
-};
+  const getOtherSpheres = async () => {
+    const req = await fetch("http://localhost:3000/");
+    otherSpheres = (await req.json()).filter((sphere) => sphere.id !== id);
+  };
 
-setupSize();
-window.addEventListener("resize", setupSize);
-
-/* CANVAS */
-const FPS = 50;
-const PARTICLES = [];
-const PARTICLES_PER_LAYER = 60;
-
-const generateColorOnRadius = (radius) => {
-  const rndDelta = Math.floor(Math.random() * 50);
-  return `rgb(${255 - radius - rndDelta}, ${radius + rndDelta}, 0)`;
-};
-
-for (let i = 0; i < 20; ++i) {
-  for (let j = 0; j < PARTICLES_PER_LAYER; ++j) {
-    const radius = i * 10 + Math.floor(Math.random() * 20);
-    PARTICLES.push({
-      radius: radius,
-      pos: j,
-      color: generateColorOnRadius(radius / 1.5),
+  const initConnection = async () => {
+    const req = await fetch("http://localhost:3000/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        sphereX: deltaX,
+        sphereY: deltaY,
+      }),
     });
+    const res = await req.json();
+    console.log(res);
+    id = res.data.id;
+  };
+
+  const updatePosition = () => {
+    fetch("http://localhost:3000/update", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id,
+        sphereX: deltaX,
+        sphereY: deltaY,
+      }),
+    });
+  };
+
+  const closeConnection = async () => {
+    try {
+      await fetch("http://localhost:3000/remove", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id,
+        }),
+      });
+    } catch (e) {
+      console.log(e);
+    }
+    return;
+  };
+
+  setupSize();
+  window.addEventListener("resize", setupSize);
+  window.addEventListener("beforeunload", closeConnection);
+
+  await initConnection();
+
+  /* CANVAS */
+  const FPS = 50;
+  const PARTICLES = [];
+  const PARTICLES_PER_LAYER = 60;
+
+  const generateColorOnRadius = (radius) => {
+    const rndDelta = Math.floor(Math.random() * 50);
+    return `rgb(${255 - radius - rndDelta}, ${radius + rndDelta}, 0)`;
+  };
+
+  for (let i = 0; i < 20; ++i) {
+    for (let j = 0; j < PARTICLES_PER_LAYER; ++j) {
+      const radius = i * 10 + Math.floor(Math.random() * 20);
+      PARTICLES.push({
+        radius: radius,
+        pos: j,
+        color: generateColorOnRadius(radius / 1.5),
+      });
+    }
   }
-}
 
-setInterval(() => {
-  ctx.clearRect(0, 0, width, height);
+  setInterval(() => {
+    // Backend call
+    getOtherSpheres();
+    updatePosition();
+  }, 1000);
 
-  // Background
-  ctx.fillStyle = "#000";
-  ctx.fillRect(0, 0, width, height);
+  setInterval(() => {
+    ctx.clearRect(0, 0, width, height);
 
-  for (const particle of PARTICLES) {
-    particle.pos += (1 / particle.radius) * 15;
+    // Background
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, width, height);
 
-    const delta = ((Math.PI * 2) / PARTICLES_PER_LAYER) * particle.pos;
-    const x = Math.cos(delta) * particle.radius;
-    const y = Math.sin(delta) * particle.radius;
+    // Particles
+    for (const particle of PARTICLES) {
+      particle.pos += (1 / particle.radius) * 15;
 
-    ctx.beginPath();
-    ctx.fillStyle = particle.color;
-    ctx.arc(
-      window.screen.width / 2 - window.screenX + x,
-      window.screen.height / 2 - window.screenY + y,
-      2,
-      0,
-      2 * Math.PI
-    );
-    ctx.fill();
-  }
-}, 1000 / FPS);
+      const delta = ((Math.PI * 2) / PARTICLES_PER_LAYER) * particle.pos;
+      const x = Math.cos(delta) * particle.radius;
+      const y = Math.sin(delta) * particle.radius;
+
+      ctx.beginPath();
+      ctx.fillStyle = particle.color;
+      ctx.arc(
+        deltaX - window.screenX + x,
+        deltaY - window.screenY + y,
+        2,
+        0,
+        2 * Math.PI
+      );
+      ctx.fill();
+    }
+
+    // Line to other spheres
+    for (const sphere of otherSpheres) {
+      ctx.beginPath();
+      ctx.strokeStyle = "#ff0000";
+      ctx.moveTo(deltaX - window.screenX, deltaY - window.screenY);
+      ctx.lineTo(
+        sphere.sphereX - window.screenX,
+        sphere.sphereY - window.screenY
+      );
+      ctx.stroke();
+    }
+  }, 1000 / FPS);
+})();
